313a5df98ddffcb360ec0988b02c6c44ef0a6ea7
dnarino <dnarino>
2026-06-02 12:54:28 -0500

    Use .env for API keys and update requirements

--- api/weather_api.py
+++ api/weather_api.py
@@ -1,4 +1,12 @@
+import os
 import fastapi
+
+from dotenv import load_dotenv
+
+# Load variables from .env file
+load_dotenv()
+# Retrieve the secret key
+API_KEY = os.getenv("WEATHER_API_KEY")
 
 router =fastapi.APIRouter()
 
